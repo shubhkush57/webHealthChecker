0438f21573b2523541f18eb8d771183a494b71f2
Shubahm <shubham.20205157@mnnit.ac.in>
2025-01-17 12:23:19 +0530

    added go routines

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 	}
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link)
 	}
 }
 
 func checkLink(link string) {
-	_,err := http.Get(link)
+	_,err := http.Get(link) // blocking call so got frozen on this line
 	if err != nil{
 		fmt.Println(link,"might be down !")
 		return 
@@ -27,3 +27,14 @@ func checkLink(link string) {
 	fmt.Println(link ,"is up !")
 	return
 }
+//we have to add go routines.
+
+/* go automatically creates one go routine by deafult. 
+To create a new go routine we write 'go' before that function
+so go create a new thread....go routine.
+So go scheduler works on one CPU (may be many CPU) and handle the different go routines.
+If one what to at a time . -> one CPU
+assign different routine to different cpu.
+
+Concurrency is not parallism
+*/
